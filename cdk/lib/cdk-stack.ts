c42349443b8af29dca4146d3562a7a91126544b9
Matt McClatchie <matthewmcclatchie@me.com>
2022-01-28 15:55:46 +0000

    set up s3 bucket

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,8 +1,30 @@
-import { Stack, StackProps } from "aws-cdk-lib"
+import {
+  Stack,
+  StackProps,
+  RemovalPolicy,
+  CfnOutput,
+  aws_s3 as s3,
+} from "aws-cdk-lib"
 import { Construct } from "constructs"
 
 export class WeddingSiteStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props)
+
+    // S3 Bucket
+    const weddingSiteBucket = new s3.Bucket(this, "WeddingSiteBucket", {
+      websiteIndexDocument: "index.html",
+      autoDeleteObjects: true,
+      removalPolicy: RemovalPolicy.DESTROY,
+    })
+
+    // Outputs
+    new CfnOutput(this, "WeddingSiteBucketDomainName", {
+      value: weddingSiteBucket.bucketDomainName,
+    })
+
+    new CfnOutput(this, "WeddingSiteBucketWebsiteUrl", {
+      value: weddingSiteBucket.bucketWebsiteUrl,
+    })
   }
 }
