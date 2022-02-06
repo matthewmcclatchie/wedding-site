5cd5babaf3589334c8a6243b9c47a536df26a5ed
Matt McClatchie <matthewmcclatchie@me.com>
2022-02-06 12:39:03 +0000

    add basic feature toggle

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,16 @@ import { RsvpForm } from "./components/RsvpForm"
 import "./App.css"
 
 function App() {
-  return (
-    <div className="App">
-      <RsvpForm />
-    </div>
-  )
+  const siteReady = process.env.REACT_APP_SITE_READY === "true"
+  if (siteReady) {
+    return (
+      <div className="App">
+        <RsvpForm />
+      </div>
+    )
+  }
+
+  return <h1>Site coming soon</h1>
 }
 
 export default App
