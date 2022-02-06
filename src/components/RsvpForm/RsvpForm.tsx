d80a62ba17cfdf1347161c0bd15040e6136c481e
Matt McClatchie <matthewmcclatchie@me.com>
2022-02-06 16:16:55 +0000

    hook up rsvp form to lambda

--- src/components/RsvpForm/RsvpForm.tsx
+++ src/components/RsvpForm/RsvpForm.tsx
@@ -2,6 +2,7 @@ import { nanoid } from "nanoid"
 import { useState } from "react"
 import { useForm } from "react-hook-form"
 import { Rsvp } from "../Rsvp"
+import { prepareRsvpData } from "../../utils/rsvp"
 
 export const RsvpForm: React.FC = () => {
   const {
@@ -13,9 +14,26 @@ export const RsvpForm: React.FC = () => {
   } = useForm()
   const [rsvps, setRsvps] = useState<string[]>([])
 
-  const onSubmit = (data: any) => {
-    console.log(data)
-    reset()
+  const [fetchStatus, setFetchStatus] = useState<FetchStatus>("initial")
+
+  const onSubmit = async (data: any) => {
+    setFetchStatus("pending")
+
+    try {
+      await fetch(`${process.env.REACT_APP_CLOUDFRONT_URL}/rsvp`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(prepareRsvpData(data)),
+      })
+
+      setFetchStatus("success")
+      reset()
+    } catch (error) {
+      console.log("error", error)
+      setFetchStatus("error")
+    }
   }
 
   const addRsvp = () => {
@@ -25,6 +43,17 @@ export const RsvpForm: React.FC = () => {
   return (
     <div className="App">
       <button onClick={addRsvp}>add rsvp</button>
+
+      {fetchStatus === "pending" && <p>Is currently loading...</p>}
+
+      {fetchStatus === "success" && <p>RSVPs submitted successfully</p>}
+
+      {fetchStatus === "error" && (
+        <p>
+          There was a problem, please check the form and try again, or contact
+          Matt or Steph
+        </p>
+      )}
 
       <form onSubmit={handleSubmit(onSubmit)}>
         {rsvps.map((rsvp) => {
@@ -39,7 +68,9 @@ export const RsvpForm: React.FC = () => {
           )
         })}
 
-        <button type="submit">click</button>
+        <button disabled={fetchStatus === "pending"} type="submit">
+          click
+        </button>
       </form>
     </div>
   )
