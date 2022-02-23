4471f3d7ffaacff9322e5a8abf6ce9ed970d323a
Matt McClatchie <matthewmcclatchie@me.com>
2022-02-23 17:52:45 +0000

    set up rsvp form components

--- src/components/Rsvp/Rsvp.tsx
+++ src/components/Rsvp/Rsvp.tsx
@@ -1,12 +1,17 @@
-import React, { useState } from "react"
+import { useState } from "react"
+import classNames from "classnames"
 import { FieldValues, FieldErrors, UseFormRegister } from "react-hook-form"
 import { SIMPLE_EMAIL_REGEX } from "../../utils/email"
+import styles from "./Rsvp.module.css"
 
 interface RsvpProps {
   id: string
   register: UseFormRegister<FieldValues>
   errors: FieldErrors
-  trigger: (id: string) => Promise<boolean>
+  trigger: (id?: string) => Promise<boolean>
+  handleRemove: (id: string) => void
+  getValues: (field: string) => string | boolean
+  index: number
 }
 
 export const Rsvp: React.FC<RsvpProps> = ({
@@ -14,120 +19,248 @@ export const Rsvp: React.FC<RsvpProps> = ({
   register,
   errors,
   trigger,
+  handleRemove,
+  getValues,
+  index,
 }) => {
   const [attending, setAttending] = useState<boolean>(false)
+  const [expanded, setExpanded] = useState<boolean>(true)
 
   const handleAttending = (event: React.ChangeEvent<HTMLInputElement>) => {
     setAttending(event.target.value === "true" ? true : false)
     trigger(`${id}.attending`)
   }
 
+  const handleExpand = () => {
+    setExpanded(!expanded)
+  }
+
+  const guestName = Boolean(getValues(`${id}.name`))
+    ? getValues(`${id}.name`)
+    : `Guest ${index + 1}`
+  const guestAttending = getValues(`${id}.attending`)
+
   return (
-    <fieldset key={id}>
-      <div>
-        <label htmlFor={`${id}.name`}>Name</label>
-        <input
-          id={`${id}.name`}
-          {...register(`${id}.name`, { required: true })}
-        />
-        {errors?.name && "name is required"}
-      </div>
-      <div>
-        <label htmlFor={`${id}.email`}>Email</label>
-        <input
-          id={`${id}.email`}
-          {...register(`${id}.email`, {
-            required: true,
-            pattern: SIMPLE_EMAIL_REGEX,
-          })}
-        />
+    <fieldset
+      key={id}
+      className={classNames(styles.wrap, {
+        [styles.wrapExpanded]: expanded,
+      })}
+    >
+      <button
+        className={classNames(styles.toggle, {
+          [styles.toggleExpanded]: expanded,
+        })}
+        type="button"
+        onClick={handleExpand}
+      >
+        {expanded ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="252"
+            height="252"
+            version="1.1"
+            viewBox="252 252 252 252"
+          >
+            <path d="m413.89 321.37 46.09-46.09 16.742 16.742-46.09 46.09h25.879v23.68h-66.301v-66.301h23.68zm-75.773 109.27-46.09 46.094-16.746-16.746 46.09-46.094h-25.879v-23.684h66.305v66.309h-23.68z" />
+          </svg>
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="252"
+            height="252"
+            viewBox="252 252 252 252"
+          >
+            <path d="m447.04 321.71-46.09 46.09-16.746-16.742 46.094-46.09h-25.879v-23.68h66.301v66.301h-23.68zM304.97 430.29l46.094-46.094 16.746 16.746-46.098 46.094h25.883v23.684h-66.309v-66.312h23.684z" />
+          </svg>
+        )}
+      </button>
 
-        {errors?.email?.type === "required" && "email is required"}
-        {errors?.email?.type === "pattern" && "doesn't look quite right"}
-      </div>
-      <div>
-        <label>Attending</label>
+      {!expanded && (
+        <div>
+          <p>
+            <strong>{guestName} </strong>
+            {guestAttending === "true" && "will be attending."}
+            {guestAttending === "false" &&
+              "will unfortunately not be attending."}
+            {!guestAttending && "has not decided if they're attending yet..."}
+          </p>
 
-        <label htmlFor={`${id}.attending.yes`}>I will be attending</label>
-        <input
-          {...register(`${id}.attending`, { required: true })}
-          type="radio"
-          value="true"
-          id={`${id}.attending.yes`}
-          onChange={handleAttending}
-        />
-
-        <label htmlFor={`${id}.attending.no`}>I won't be attending</label>
-        <input
-          {...register(`${id}.attending`, { required: true })}
-          type="radio"
-          value="false"
-          id={`${id}.attending.no`}
-          onChange={handleAttending}
-        />
-
-        {errors?.attending && "please select an option"}
-      </div>
-
-      {attending && (
-        <>
-          <div>
-            <label>Meal type</label>
-
-            <label htmlFor={`${id}.meal.meat`}>Meat</label>
-            <input
-              {...register(`${id}.meal`, { required: true })}
-              type="radio"
-              value="Meat"
-              id={`${id}.meal.meat`}
-            />
-
-            <label htmlFor={`${id}.meal.vegetarian`}>Vegetarian</label>
-            <input
-              {...register(`${id}.meal`, { required: true })}
-              type="radio"
-              value="Vegetarian"
-              id={`${id}.meal.vegetarian`}
-            />
-
-            <label htmlFor={`${id}.meal.vegan`}>Vegan</label>
-            <input
-              {...register(`${id}.meal`, { required: true })}
-              type="radio"
-              value="Vegan"
-              id={`${id}.meal.vegan`}
-            />
-
-            <label htmlFor={`${id}.meal.childrens`}>Childrens</label>
-            <input
-              {...register(`${id}.meal`, { required: true })}
-              type="radio"
-              value="Childrens"
-              id={`${id}.meal.childrens`}
-            />
-
-            {errors?.meal && "please select an option"}
-          </div>
-
-          <div>
-            <label htmlFor={`${id}.dietary`}>Dietary requirements</label>
-            <input id={`${id}.dietary`} {...register(`${id}.dietary`)} />
-          </div>
-
-          <div>
-            <label htmlFor={`${id}.song`}>Song</label>
-            <input id={`${id}.song`} {...register(`${id}.song`)} />
-          </div>
-
-          <div>
-            <label htmlFor={`${id}.question`}>Question</label>
-            <textarea
-              id={`${id}.question`}
-              {...register(`${id}.question`)}
-            ></textarea>
-          </div>
-        </>
+          {errors && (
+            <p>
+              There is a problem with the info for this guest, please review...
+            </p>
+          )}
+        </div>
       )}
+
+      <button
+        className={styles.remove}
+        type="button"
+        onClick={() => handleRemove(id)}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+        >
+          <path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z" />
+        </svg>
+      </button>
+
+      <div
+        className={classNames(styles.fields, {
+          [styles.fieldsExpanded]: expanded,
+          [styles.fieldsCollapsed]: !expanded,
+        })}
+      >
+        <div className={styles.field}>
+          <label htmlFor={`${id}.name`}>Guest name</label>
+
+          <input
+            type="text"
+            id={`${id}.name`}
+            {...register(`${id}.name`, { required: true })}
+          />
+          {errors?.name && (
+            <p className={styles.errorText}>👆 Guest name is required</p>
+          )}
+        </div>
+
+        <div className={styles.field}>
+          <label htmlFor={`${id}.email`}>Email</label>
+          <input
+            type="email"
+            id={`${id}.email`}
+            {...register(`${id}.email`, {
+              required: true,
+              pattern: SIMPLE_EMAIL_REGEX,
+            })}
+          />
+
+          {errors?.email?.type === "required" && (
+            <p className={styles.errorText}>👆 Guest email is required</p>
+          )}
+          {errors?.email?.type === "pattern" && (
+            <p className={styles.errorText}>
+              🤔 Please check the email, it doesn't look quite right...
+            </p>
+          )}
+        </div>
+
+        <div className={classNames(styles.field, styles.fieldRadio)}>
+          <div>
+            <label className={styles.fieldRadioLabel}>
+              Will you be attending?
+            </label>
+            {errors?.attending && (
+              <p className={styles.errorText}>
+                Please select an option &nbsp;👉
+              </p>
+            )}
+          </div>
+
+          <div className={styles.fieldRadioInputs}>
+            <input
+              {...register(`${id}.attending`, { required: true })}
+              type="radio"
+              value="true"
+              id={`${id}.attending.yes`}
+              onChange={handleAttending}
+            />
+            <label htmlFor={`${id}.attending.yes`}>🎉 &nbsp;Yes</label>
+
+            <input
+              {...register(`${id}.attending`, { required: true })}
+              type="radio"
+              value="false"
+              id={`${id}.attending.no`}
+              onChange={handleAttending}
+            />
+            <label htmlFor={`${id}.attending.no`}>😢 &nbsp;No</label>
+          </div>
+        </div>
+
+        {attending && (
+          <>
+            <div
+              className={classNames(
+                styles.field,
+                styles.fieldRadio,
+                styles.fieldRadioLong
+              )}
+            >
+              <div>
+                <label className={styles.fieldRadioLabel}>
+                  Meal preference
+                </label>
+                {errors?.meal && (
+                  <p className={styles.errorText}>
+                    Please select your preference &nbsp;👆
+                  </p>
+                )}
+              </div>
+
+              <div className={styles.fieldRadioInputs}>
+                <input
+                  {...register(`${id}.meal`, { required: true })}
+                  type="radio"
+                  value="Meat"
+                  id={`${id}.meal.meat`}
+                />
+                <label htmlFor={`${id}.meal.meat`}>Meat</label>
+
+                <input
+                  {...register(`${id}.meal`, { required: true })}
+                  type="radio"
+                  value="Veggie"
+                  id={`${id}.meal.veggie`}
+                />
+                <label htmlFor={`${id}.meal.veggie`}>Veggie</label>
+
+                <input
+                  {...register(`${id}.meal`, { required: true })}
+                  type="radio"
+                  value="Vegan"
+                  id={`${id}.meal.vegan`}
+                />
+                <label htmlFor={`${id}.meal.vegan`}>Vegan</label>
+
+                <input
+                  {...register(`${id}.meal`, { required: true })}
+                  type="radio"
+                  value="Kids"
+                  id={`${id}.meal.kids`}
+                />
+                <label htmlFor={`${id}.meal.kids`}>Kids</label>
+              </div>
+            </div>
+
+            <div className={styles.field}>
+              <label htmlFor={`${id}.dietary`}>Dietary requirements</label>
+
+              <input
+                type="text"
+                id={`${id}.dietary`}
+                {...register(`${id}.dietary`)}
+                placeholder="Allergies, intollerances, etc..."
+              />
+            </div>
+
+            <div className={styles.field}>
+              <label htmlFor={`${id}.song`}>Reception song request</label>
+
+              <input
+                type="text"
+                id={`${id}.song`}
+                {...register(`${id}.song`)}
+                placeholder="What would you love to hear?"
+              />
+            </div>
+          </>
+        )}
+      </div>
     </fieldset>
   )
 }
--- src/components/RsvpForm/RsvpForm.tsx
+++ src/components/RsvpForm/RsvpForm.tsx
@@ -1,8 +1,10 @@
+import classNames from "classnames"
 import { nanoid } from "nanoid"
 import { useState } from "react"
 import { useForm } from "react-hook-form"
 import { Rsvp } from "../Rsvp"
 import { prepareRsvpData } from "../../utils/rsvp"
+import styles from "./RsvpForm.module.css"
 
 export const RsvpForm: React.FC = () => {
   const {
@@ -10,11 +12,12 @@ export const RsvpForm: React.FC = () => {
     register,
     trigger,
     reset,
+    getValues,
     formState: { errors },
   } = useForm()
   const [rsvps, setRsvps] = useState<string[]>([])
-
   const [fetchStatus, setFetchStatus] = useState<FetchStatus>("initial")
+  const hasErrors = Object.keys(errors).length > 0
 
   const onSubmit = async (data: any) => {
     setFetchStatus("pending")
@@ -40,39 +43,86 @@ export const RsvpForm: React.FC = () => {
     setRsvps([...rsvps, nanoid()])
   }
 
+  const removeRsvp = (target: string) => {
+    setRsvps(rsvps.filter((rsvp) => rsvp !== target))
+  }
+
   return (
-    <div>
-      <button onClick={addRsvp}>add rsvp</button>
+    <section className={styles.background}>
+      <div className={styles.wrapper}>
+        <div className={styles.intro}>
+          <h2 className={styles.title}>RSVP</h2>
+          <p>
+            We'd love to have our numbers confirmed by{" "}
+            <strong>June 2022</strong>.
+          </p>
+          <p>Please respond for each member of your party in the form.</p>
+          <p>Contact Matt or Steph if you have any problems.</p>
+          <p>We can't wait to see you!</p>
+        </div>
 
-      {fetchStatus === "pending" && <p>Is currently loading...</p>}
+        <div className={styles.main}>
+          {fetchStatus === "pending" && <p>Is currently loading...</p>}
 
-      {fetchStatus === "success" && <p>RSVPs submitted successfully</p>}
+          {fetchStatus === "success" && <p>RSVPs submitted successfully</p>}
 
-      {fetchStatus === "error" && (
-        <p>
-          There was a problem, please check the form and try again, or contact
-          Matt or Steph
-        </p>
-      )}
+          {fetchStatus === "error" && (
+            <p>
+              There was a problem, please check the form and try again, or
+              contact Matt or Steph
+            </p>
+          )}
 
-      <form onSubmit={handleSubmit(onSubmit)}>
-        {rsvps.map((rsvp) => {
-          return (
-            <Rsvp
-              key={rsvp}
-              id={rsvp}
-              register={register}
-              errors={errors[rsvp]}
-              trigger={trigger}
-            />
-          )
-        })}
+          <form noValidate onSubmit={handleSubmit(onSubmit)}>
+            {rsvps.map((rsvp, index) => {
+              return (
+                <Rsvp
+                  key={rsvp}
+                  id={rsvp}
+                  register={register}
+                  errors={errors[rsvp]}
+                  trigger={trigger}
+                  handleRemove={removeRsvp}
+                  getValues={getValues}
+                  index={index}
+                />
+              )
+            })}
 
-        <button disabled={fetchStatus === "pending"} type="submit">
-          click
-        </button>
-      </form>
-    </div>
+            {hasErrors && (
+              <p className={styles.errors}>
+                Please fix the errors above to submit
+              </p>
+            )}
+
+            <div
+              className={classNames({
+                [styles.controlsMutliple]: Boolean(rsvps.length),
+              })}
+            >
+              <button
+                type="button"
+                className={styles.controlAdd}
+                onClick={addRsvp}
+              >
+                {rsvps.length ? "Add another guest" : "Add a guest"}
+              </button>
+              {Boolean(rsvps.length) && (
+                <button
+                  className={styles.controlSubmit}
+                  disabled={fetchStatus === "pending"}
+                  type="submit"
+                >
+                  {rsvps.length > 1
+                    ? `Send ${rsvps.length} RSVPs`
+                    : "Send RSVP"}
+                </button>
+              )}
+            </div>
+          </form>
+        </div>
+      </div>
+    </section>
   )
 }
 
