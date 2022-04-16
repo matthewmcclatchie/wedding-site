034c3c081c0c0831c69a43f3f92657125613adae
Matt McClatchie <matthewmcclatchie@me.com>
2022-04-16 12:33:57 +0100

    simplify contact section

--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -1,79 +1,18 @@
-import { useState } from "react"
-import { useForm } from "react-hook-form"
-import { SIMPLE_EMAIL_REGEX } from "../../utils/email"
+import styles from "./ContactForm.module.css"
 
 export const ContactForm: React.FC = () => {
-  const {
-    handleSubmit,
-    register,
-    reset,
-    formState: { errors },
-  } = useForm()
-  const [fetchStatus, setFetchStatus] = useState<FetchStatus>("initial")
-
-  const onSubmit = async (data: any) => {
-    setFetchStatus("pending")
-
-    try {
-      await fetch(`${process.env.REACT_APP_CLOUDFRONT_URL}/contact`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-      })
-
-      setFetchStatus("success")
-      reset()
-    } catch (error) {
-      console.log("error", error)
-      setFetchStatus("error")
-    }
-  }
-
   return (
-    <div>
-      {fetchStatus === "pending" && <p>Is currently loading...</p>}
-
-      {fetchStatus === "success" && <p>Form submitted successfully</p>}
-
-      {fetchStatus === "error" && (
+    <div id="contact" className={styles.background}>
+      <div className={styles.wrapper}>
+        <div className={styles.emoji}>👍</div>
+        <p>Thanks for visiting - We can't wait to see you!</p>
         <p>
-          There was a problem, please check the form and try again, or contact
-          Matt or Steph
+          Have a question?{" "}
+          <a href="mailto:mattandstephgetwed@gmail.com">Email us</a> or message
+          on Whatsapp
         </p>
-      )}
-
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div>
-          <label htmlFor="name">Name</label>
-          <input id="name" {...register("name", { required: true })} />
-          {errors?.name && "name is required"}
-        </div>
-
-        <div>
-          <label htmlFor="email">Email</label>
-          <input
-            id="email"
-            {...register("email", {
-              required: true,
-              pattern: SIMPLE_EMAIL_REGEX,
-            })}
-          />
-
-          {errors?.email?.type === "required" && "email is required"}
-          {errors?.email?.type === "pattern" && "doesn't look quite right"}
-        </div>
-
-        <div>
-          <label htmlFor="question">Question</label>
-          <textarea id="question" {...register("message")}></textarea>
-        </div>
-
-        <button disabled={fetchStatus === "pending"} type="submit">
-          click
-        </button>
-      </form>
+        <p>Steph + Matt xx</p>
+      </div>
     </div>
   )
 }
