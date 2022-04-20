1e3d7bb6f3dc3e91ddae537cbc5a65b7f47e2cd1
Matt McClatchie <matthewmcclatchie@me.com>
2022-04-20 18:09:30 +0100

    update gift section

--- src/components/Gifts/Gifts.tsx
+++ src/components/Gifts/Gifts.tsx
@@ -9,31 +9,45 @@ export const Gifts: React.FC = () => {
         </div>
 
         <div className={styles.info}>
+          <p>Oh hey! Just a little note on gifts...</p>
           <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi
-            cumque quod quaerat atque maiores sapiente voluptatem assumenda?
-            Earum modi, exercitationem id odit rerum dolorum! Modi beatae harum
-            ut et consectetur!
+            Having our closest friends and family with us to celebrate on our
+            wedding day is all we need and we really cannot wait. We really
+            appreciate that some of you are travelling a{" "}
+            <strong>
+              <em>very</em>
+            </strong>{" "}
+            long way to be with us and it really does mean the world - so thank
+            you so much.
           </p>
-
           <p>
-            Lorem ipsum, <strong>dolor sit amet</strong> consectetur adipisicing
-            elit. Quasi cumque quod quaerat atque maiores sapiente voluptatem
-            assumenda? Earum modi, exercitationem id odit rerum dolorum! Modi
-            beatae harum ut et consectetur!
+            In terms of <em>things...</em> we've lived together (😮 out of
+            wedlock!) for a long time now and are fortunate enough to have
+            pretty much everything that we need to get by.
           </p>
+          <p>So...</p>
+          <p>
+            You'll know that we love to travel and experience new things, so
+            have decided that for a honeymoon, we'd like to do a road-trip
+            across California. Neither of us have been to part of America before
+            and it feels like a once in a lifetime opportunity for us. We've got
+            a good idea of what it will cost and we're funding it ourselves.
+          </p>
+          <p>
+            You being there with us on the day is more than enough. Should
+            anybody <em>really</em> want to send a gift, then any contribution
+            toward our trip would be heartfeltly appreciated. We hope to have a
+            truly unforgetable time and anything on top of what we're saving for
+            would honestly be a massive bonus for us.
+          </p>
+          <p>Thank you very much x</p>
         </div>
 
-        <div className={styles.footer}>
-          <a
-            href="https://www.google.com/maps/dir//Middleton+Lodge+Estate,+Kneeton+Ln,+Middleton+Tyas,+Richmond+DL10+6NJ/@54.4565315,-1.6610952,17z/data=!4m9!4m8!1m0!1m5!1m1!1s0x487e9fb0027c20cb:0xa31baf2d13a285e9!2m2!1d-1.6589012!2d54.4565315!3e0"
-            target="_blank"
-            rel="noreferrer"
-            className={styles.cta}
-          >
-            Gift CTA
+        {/* <div className={styles.footer}>
+          <a href="#" target="_blank" rel="noreferrer" className={styles.cta}>
+            Add to travel fund
           </a>
-        </div>
+        </div> */}
       </div>
     </div>
   )
